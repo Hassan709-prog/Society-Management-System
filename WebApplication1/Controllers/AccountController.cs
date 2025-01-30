44d40ac4fb9de5dbe2ba5a6cb10d7898f011141c
Muhammad Hassan <hassancs709@gmail.com>
2025-01-30 15:44:53 -0500

    Add project files.

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocietyManagementSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult MemberLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult MemberLogin(string username, string password)
+        {
+            if (username == "admin" && password == "password")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid Username or Password!";
+                return View();
+            }
+        }
+
+        public ActionResult AdminLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AdminLogin(string username, string password)
+        {
+            if (username == "admin" && password == "admin123")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid Username or Password!";
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Controllers/SignupController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class SignupController : Controller
+    {
+        public ActionResult Signuppage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signuppage(string name, string house_number, string email, string password)
+        {
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(house_number) || string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                ViewBag.ErrorMessage = "All fields are required!";
+                return View();
+            }
+
+            if (!IsValidEmail(email))
+            {
+                ViewBag.ErrorMessage = "Please enter a valid email address.";
+                return View();
+            }
+
+            
+            ViewBag.SuccessMessage = "You have successfully signed up!";
+
+            return View();  
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var mail = new System.Net.Mail.MailAddress(email);
+                return mail.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
