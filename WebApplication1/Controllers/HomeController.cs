fe0d4ae47aed7874516516b9039385c2ad9f41d5
Muhammad Hassan <hassancs709@gmail.com>
2025-02-01 07:26:32 -0500

    write about css

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -17,13 +18,57 @@ namespace SocietyManagementSystemControllers
         {
             return View();
         }
-        public ActionResult Contact()
-        {
-            return View();
-        }
+      
+            public ActionResult Contact()
+            {
+                return View();
+            }
         public ActionResult About()
         {
             return View();
         }
+
+        [HttpPost]
+            public ActionResult SubmitContactForm(string name, string email, string message)
+            {
+                try
+                {
+                 
+                    SendEmail(name, email, message);
+
+             
+                    ViewBag.Message = "Your message has been sent successfully!";
+                }
+                catch (Exception ex)
+                {
+        
+                    ViewBag.Message = "There was an error sending your message. Please try again.";
+                    Console.WriteLine(ex.Message);
+                }
+
+                return View("Index"); 
+            }
+
+   
+            private void SendEmail(string name, string email, string message)
+            {
+                MailMessage mail = new MailMessage();
+                mail.To.Add("support@societymanagement.com"); 
+                mail.From = new MailAddress(email);
+                mail.Subject = "New Contact Form Submission";
+                mail.Body = $"<p><b>Name:</b> {name}</p><p><b>Email:</b> {email}</p><p><b>Message:</b> {message}</p>";
+                mail.IsBodyHtml = true;
+
+                SmtpClient smtp = new SmtpClient
+                {
+                    Host = "smtp.yourmailserver.com", 
+                    Port = 587, 
+                    Credentials = new System.Net.NetworkCredential("your-email@example.com", "your-password"),
+                    EnableSsl = true
+                };
+
+                smtp.Send(mail);
+            }
+
     }
 }
--- WebApplication1/Models/usermodel.cs
+++ WebApplication1/Models/usermodel.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace SocietyManagementSystem.Models
+namespace WebApplication1.Models
 {
-    public class UserModel
+    public class usermodel
     {
+        public int Userid { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string Email { get; set; }
     }
 }
